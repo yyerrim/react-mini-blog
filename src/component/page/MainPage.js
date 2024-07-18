c4075e6be1a9686ecd69cfa4f3105dc7833c0b1e
yerm <leeyelim98@gmail.com>
2024-07-18 11:09:03 +0900

    post-list

--- src/component/page/MainPage.js
+++ src/component/page/MainPage.js
@@ -2,7 +2,8 @@ import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import PostList from '../list/PostList';
 import Button from '../ui/Button';
-import data from '../../data.json';
+import { useEffect, useState } from 'react';
+// import data from '../../data.json';
 
 const Wrapper = styled.div`
   padding: 16px;
@@ -26,25 +27,36 @@ function MainPage(props) {
   const navigate = useNavigate();
   // useNavigate : NavLink, Link와 같은 역할
 
-  return (
-    <Wrapper>
-      <Container>
-        <Button
-          title='글 작성하기'
-          onClick={() => {
-            navigate('/post-write');
-          }}
-        />
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function get() {
+      const url = 'http://127.0.0.1:8080/post-list';
+      const res = await fetch(url));
+  const data = await res.json();
+  setData(data);
+}
+get();
+  }, []);
 
-        <PostList
-          posts={data}
-          onClickItem={(item) => {
-            navigate(`/post/${item.id}`);
-          }}
-        />
-      </Container>
-    </Wrapper>
-  );
+return (
+  <Wrapper>
+    <Container>
+      <Button
+        title='글 작성하기'
+        onClick={() => {
+          navigate('/post-write');
+        }}
+      />
+
+      <PostList
+        posts={data}
+        onClickItem={(item) => {
+          navigate(`/post/${item.id}`);
+        }}
+      />
+    </Container>
+  </Wrapper>
+);
 }
 
 export default MainPage;
